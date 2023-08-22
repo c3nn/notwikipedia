700484d45aab18b2222e59f334cfc76daeb01829
Conmann <hello@c3n.nl>
2023-08-21 22:09:53 -0500

    added one slash to fix the whole issue
    i hate my life

--- 404.js
+++ 404.js
@@ -1,6 +1,6 @@
 let url = window.location.href.replace("https://","").replace("http://","").replace("notwikipedia.org/","")
 if(!url.startsWith("wiki/")){
-	window.location.replace("http://notwikipedia.org/404");
+	window.location.replace("http://notwikipedia.org/404/");
 }
 
 console.log(url)
