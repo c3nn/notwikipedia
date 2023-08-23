9a033061fc5ea4e0e9c0af5c2b65ca215ce24263
Conmann <hello@c3n.nl>
2023-08-23 00:26:37 -0500

    got most things working

--- 404.js
+++ 404.js
@@ -1,6 +1,14 @@
 let url = window.location.href.replace("https://","").replace("http://","").replace("notwikipedia.org/","")
-if(!url.startsWith("wiki/")){
+let urlParams = new URLSearchParams(window.location.search) // dev
+if((!url.startsWith("wiki/") || url.replace("wiki/","") == "") && !urlParams.has('dev')){ // dev
 	window.location.replace("http://notwikipedia.org/404/");
 }
+url = url.replace("wiki/","")
+document.querySelectorAll('.JSPutURL').forEach((element) => {
+	element.innerHTML += url;
+});
+document.querySelectorAll('.JSLinkURL').forEach((element) => {
+	element.href = 'https://wikipedia.org/wiki/' + url;
+});
 
 console.log(url)
